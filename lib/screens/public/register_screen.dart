1c1740b32355dfa3c87a8665373fb1b59d01aeb1
Erick Sebastian Caiza Quishpe <caizaquishpeericksebastian@gmail.com>
2025-04-07 21:01:37 -0500

    Actualización en los intrefaces : Login y Registro

--- lib/screens/public/register_screen.dart
+++ lib/screens/public/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hotels/screens/public/login_screen.dart';
 import 'package:hotels/services/auth_service.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -20,24 +21,34 @@ class _RegisterScreenState extends State<RegisterScreen> {
     'password': TextEditingController(),
     'diaNacimiento': TextEditingController(),
   };
+  
+  final Map<String, FocusNode> _focusNodes = {
+    'nombre': FocusNode(),
+    'apellido': FocusNode(),
+    'cedula': FocusNode(),
+    'telefono': FocusNode(),
+    'email': FocusNode(),
+    'password': FocusNode(),
+    'fechaNacimiento': FocusNode(),
+  };
 
   bool _isPasswordVisible = false;
   bool loading = false;
   final authService = AuthService();
-
-  final FocusNode _emailFocusNode = FocusNode();
-  final FocusNode _passwordFocusNode = FocusNode();
 
   @override
   void dispose() {
     _controllers.values.forEach((controller) {
       controller.dispose();
     });
-    _emailFocusNode.dispose();
-    _passwordFocusNode.dispose();
+    _focusNodes.values.forEach((focusNode) {
+      focusNode.dispose();
+    });
     super.dispose();
   }
-
+  void goToLogin(){
+    
+  }
   void register() async {
     if (!_formKey.currentState!.validate()) return;
     setState(() => loading = true);
@@ -67,183 +78,146 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Text(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: Text(
                 'Registrarse',
                 style: TextStyle(
                   fontSize: 44,
-                  fontWeight: FontWeight.w900,
-                  color: Color(0xFF001D5A),
-                ),
+                  fontWeight: FontWeight.bold,
+                  color: colorPrimary ,                ),
               ),
-              const SizedBox(height: 20),
-              _buildTextFormField('nombre', 'Nombre'),
-              _buildTextFormField('apellido', 'Apellido'),
-              _buildTextFormField('cedula', 'Número de cédula'),
-              _buildTextFormField('telefono', 'Número de telefono'),
-              _buildTextFormField('email', 'Correo electrónico', focusNode: _emailFocusNode),
-              _buildFieldPassword('password', 'Contraseña'),
-              _buildTextFormField('diaNacimiento', 'Fecha de Nacimiento'),
-              const SizedBox(height: 20),
-              Center(
-                child: loading
-                    ? const CircularProgressIndicator()
-                    : ElevatedButton(
-                        onPressed: register,
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(horizontal: 90, vertical: 18),
-                          backgroundColor: const Color(0xFF021337),
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
+            ),
+            const SizedBox(height: 20),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  _buildTextFormField('nombre', 'Nombre',icon: Icon(Icons.account_circle, color: colorPrimary)),
+                  _buildTextFormField('apellido', 'Apellido',icon: Icon(Icons.account_circle, color: colorPrimary)),
+                  _buildTextFormField('cedula', 'Número de cédula',icon: Icon(Icons.numbers, color: colorPrimary)),
+                  _buildTextFormField('telefono', 'Número de teléfono',icon: Icon(Icons.numbers, color: colorPrimary)),
+                  _buildTextFormField('email', 'Correo electrónico', focusNode: _focusNodes['email'],icon: Icon(Icons.email, color: colorPrimary)),
+                  _buildPasswordField('password', 'Contraseña'),
+                  _buildTextFormField('diaNacimiento', 'Fecha de Nacimiento',icon: Icon(Icons.date_range, color: colorPrimary)),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: loading
+                        ? const CircularProgressIndicator()
+                        : ElevatedButton(
+                            onPressed: register,
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(horizontal: 90, vertical: 18),
+                              backgroundColor: const Color(0xFF021337),
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: const Text(
+                              'Crear cuenta',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
                           ),
-                        ),
-                        child: const Text(
-                          'Crear cuenta',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
+                  ),
+                  SizedBox(height: 40),
+                  Center(
+  child: ElevatedButton(
+    onPressed: goToLogin,
+    style: ElevatedButton.styleFrom(
+      padding: const EdgeInsets.symmetric(horizontal: 90, vertical: 18),
+      backgroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+        side: BorderSide(
+          color: colorPrimary, // Color del borde
+          width: 2, // Ancho del borde
+        ),
+      ),
+    ),
+    child: Text(
+      'Cancelar',
+      style: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        color: colorPrimary,
+      ),
+    ),
+  ),
+),
+
+                  
+                ]
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Widget _buildTextFormField(String key, String labelText, {FocusNode? focusNode, bool obscureText = false}) {
-    return Stack(
-      children: [
-        TextFormField(
-          controller: _controllers[key],
-          focusNode: focusNode,
-          obscureText: obscureText,
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return 'Este campo es obligatorio';
-            }
-            return null;
-          },
-          style: const TextStyle(color: Colors.black),
-          decoration: InputDecoration(
-            contentPadding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
-            suffixIcon: Icon(
-              focusNode?.hasFocus ?? false ? Icons.email : Icons.person,
-              size: 20,
-              color: focusNode?.hasFocus ?? false ? colorPrimary : Colors.grey,
-            ),
-            filled: true,
-            fillColor: Colors.white,
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-              borderSide: const BorderSide(color: Colors.white),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-              borderSide: const BorderSide(color: Colors.white, width: 4),
-            ),
-            errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-              borderSide: BorderSide(color: colorPrimary, width: 2),
-            ),
-            focusedErrorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-              borderSide: BorderSide(color: colorPrimary, width: 2),
-            ),
-            labelText: '',
-            errorStyle: const TextStyle(color: Colors.white),
+ Widget _buildTextFormField(
+  String key, 
+  String labelText, 
+  {FocusNode? focusNode, 
+   Icon? icon}) {  // Icono personalizado como parámetro
+  return Padding(
+    padding: const EdgeInsets.only(bottom: 16.0), // Espaciado entre los campos
+    child: TextFormField(
+      controller: _controllers[key],
+      focusNode: focusNode,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20), // Ajusta el padding para alinear el icono
+        suffixIcon: Padding(
+          padding: const EdgeInsets.only(right: 10), // Ajusta el icono en relación con el borde
+          child: icon ?? Icon( // Si no se pasa un icono, se usa uno predeterminado
+            Icons.person,
+            size: 20,
+            color: focusNode?.hasFocus ?? false ? colorPrimary : Colors.grey, // Cambiar color según el foco
           ),
         ),
-        Positioned(
-          left: 20,
-          top: (focusNode?.hasFocus ?? false || _controllers[key]!.text.isNotEmpty)
-              ? 6
-              : 22,
-          child: AnimatedDefaultTextStyle(
-            duration: const Duration(milliseconds: 200),
-            style: TextStyle(
-              fontSize: (focusNode?.hasFocus ?? false || _controllers[key]!.text.isNotEmpty) ? 12 : 16,
-              color: Colors.black,
-            ),
-            child: Text(labelText),
-          ),
+        filled: true,
+        fillColor: Colors.white,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(color: Colors.white),
         ),
-      ],
-    );
-  }
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(color: Colors.white, width: 4),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide(color: colorPrimary, width: 2),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide(color: colorPrimary, width: 2),
+        ),
+        labelText: labelText,
+        labelStyle: TextStyle(color: Colors.black, fontSize: 16),
+        errorStyle: const TextStyle(color: Colors.white),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Por favor, complete el campo';
+        }
+        return null;
+      },
+    ),
+  );
+}
 
-  Widget _buildFieldPassword(String key, String labelText) {
-    return Stack(
-      children: [
-        TextFormField(
-          controller: _controllers[key],
-          obscureText: !_isPasswordVisible,
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return 'Este campo es obligatorio';
-            }
-            return null;
-          },
-          style: const TextStyle(color: Colors.black),
-          decoration: InputDecoration(
-            contentPadding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
-            suffixIcon: IconButton(
-              onPressed: () {
-                setState(() {
-                  _isPasswordVisible = !_isPasswordVisible;
-                });
-              },
-              icon: Icon(
-                _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                size: 20,
-                color: _isPasswordVisible ? colorPrimary : Colors.grey,
-              ),
-            ),
-            filled: true,
-            fillColor: Colors.white,
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-              borderSide: const BorderSide(color: Colors.white),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-              borderSide: const BorderSide(color: Colors.white, width: 4),
-            ),
-            errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-              borderSide: BorderSide(color: colorPrimary, width: 2),
-            ),
-            focusedErrorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-              borderSide: BorderSide(color: colorPrimary, width: 2),
-            ),
-            labelText: '',
-            errorStyle: const TextStyle(color: Colors.white),
-          ),
-        ),
-        Positioned(
-          left: 20,
-          top: (_isPasswordVisible || _controllers[key]!.text.isNotEmpty) ? 6 : 22,
-          child: AnimatedDefaultTextStyle(
-            duration: const Duration(milliseconds: 200),
-            style: TextStyle(
-              fontSize: (_isPasswordVisible || _controllers[key]!.text.isNotEmpty) ? 12 : 16,
-              color: Colors.black,
-            ),
-            child: const Text('Contraseña'),
-          ),
-        ),
-      ],
-    );
+  Widget _buildPasswordField(String key, String labelText) {
+    return _buildTextFormField(key, labelText, focusNode: _focusNodes[key]);
   }
 }
